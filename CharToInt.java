b9590f3cbb4cf14fc95e79768c20dd77719ac6d6
bodepparaiAmulya <105347366+bodepparaiAmulya@users.noreply.github.com>
2023-12-26 18:44:38 +0530

    Add files via upload

---
+++ CharToInt.java
@@ -0,0 +1,13 @@
+package com.capgemini.com;
+
+public class CharToInt {
+	public static void main(String args[]){  
+		char c='a';  
+		char c2='1';  
+		int a=c;  
+		int b=c2;  
+		System.out.println(a);  
+		System.out.println(b);  
+		}}  
+
+
---
+++ LongToInt.java
@@ -0,0 +1,10 @@
+package com.capgemini.com;
+
+public class LongToInt {
+	public static void main(String args[]){  
+		Long l= new Long(10);  
+		int i=l.intValue();  
+		System.out.println(i);  
+		}}  
+
+
